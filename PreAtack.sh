2ea849fb395da0880a2d0a26b2868573e29cd94b
Francesc Castell Portella <56691800+FranckyCastell@users.noreply.github.com>
2023-03-04 00:08:11 +0100

    Create PreAtack.sh

---
+++ PreAtack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Instalación de Python3 y pip3
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip
+
+# Instalación de git
+sudo apt-get install -y git
+
+# Clonar el primer repositorio
+git clone https://github.com/FranckyCastell/ArpSpoof.git
+
+# Clonar el segundo repositorio
+git clone https://github.com/FranckyCastell/Mac_Changer.git
